1b35171ed148be44a535b656a0aea275936f3b81
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-11-26 20:32:29 +0200

    Refactor main transcript into modular functions.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from langchain_community.document_loaders import YoutubeLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.faiss import FAISS
@@ -8,58 +9,55 @@ from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
 
-# Load Environment varibales
-load_dotenv()
+def instantiate_model(api_key) -> ChatOpenAI:
+    model = ChatOpenAI(api_key=api_key, model="gpt-3.5-turbo-1106", temperature=0.3)
+    return model
 
-# Step 1: Load the YouTube transcript
-url = "https://www.youtube.com/watch?v=0_guvO2MWfE"
-loader = YoutubeLoader.from_youtube_url(url)
-transcript = loader.load()
 
-# Step 2: Split the transcript document into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=100
-)
+def load_split_transcript(url):
+    loader = YoutubeLoader.from_youtube_url(url)
+    transcript = loader.load()
 
-split_transcript = text_splitter.split_documents(transcript)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    split_transcript = text_splitter.split_documents(transcript)
+    return split_transcript
 
-# Step 3: Create a vector store
-embeddings = OpenAIEmbeddings()
-vector_store = FAISS.from_documents(split_transcript, embedding=embeddings)
 
-# Step 4: Create a Retriver
-retriever = vector_store.as_retriever(search_kwargs={"k":4})
+def create_vector_store(split_transcript, api_key) -> FAISS:
+    embeddings = OpenAIEmbeddings(api_key=api_key)
+    vector_store = FAISS.from_documents(split_transcript, embedding=embeddings)
+    return vector_store
 
-# Step 5: Creat a prompt tempalate
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful AI assistant that answer questions about YouTube Videos based on the video's transcript."),
-    ("human", """Answer the following question: {input}
-    By searching the following video transcript: {context}
-    Only use the factual information from the transcript to answer the question. If you feel like you don't have enough information to answer the question, say I don't know. Your answers should be verbose and deatailed.""")
-])
 
-# Step 6: Load the model
-model = ChatOpenAI(
-    model="gpt-3.5-turbo-1106",
-    temperature=0.3
-)
+def create_retriever_chain(vector_store, model):
+    retriever = vector_store.as_retriever(search_kwargs={"k":4})
 
-# Step 7: Create a Retriver chain
-# First we have to create a document chain Then pass it to the retriever chain
-document_chain = create_stuff_documents_chain(
-    llm=model,
-    prompt=prompt
-)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful AI assistant that answer questions about YouTube Videos based on the video's transcript."),
+        ("human", """Answer the following question: {input}
+        By searching the following video transcript: {context}
+        Only use the factual information from the transcript to answer the question. If you feel like you don't have enough information to answer the question, say I don't know. Your answers should be verbose and deatailed.""")
+    ])
+    document_chain = create_stuff_documents_chain(llm=model, prompt=prompt)
+    retriever_chain = create_retrieval_chain(retriever=retriever, combine_docs_chain=document_chain)
+    return retriever_chain
 
-retriever_chain = create_retrieval_chain(
-    retriever=retriever, 
-    combine_docs_chain=document_chain
-)
 
-# Step 8: Generate a response
-response = retriever_chain.invoke({
-    "input": "What was Mohammed Hijab's opinion on divorce?"
-})
+def generate_response(retriever, question: str):
+    response = retriever.invoke({
+        "input": question
+    })
+    return response['answer']
 
-print(response['answer'])
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    model = instantiate_model(api_key)
+    url = "https://www.youtube.com/watch?v=0_guvO2MWfE"
+    split_transcript = load_split_transcript(url)
+    vector_store = create_vector_store(split_transcript, api_key)
+    retriever = create_retriever_chain(vector_store, model)
+    response = generate_response(retriever, "What was Mohammed Hijab's opinion on divorce?")
+    print(response)
